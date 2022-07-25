1514bacd53a7c4b9616e4c5855c359989c395f48
Colby Salkeld <csalkeld0507@gmail.com>
2022-07-25 12:18:53 -0500

    Added equals and compliment operator

--- Pinewood/include/Pinewood/EnumSupport.h
+++ Pinewood/include/Pinewood/EnumSupport.h
@@ -3,12 +3,19 @@
 #include <type_traits>
 
 #define PW_DEFINE_ENUMCLASS_OPERATOR_OR(enum_class)		\
-	constexpr std::underlying_type_t<enum_class> operator|(uint32_t lhs, WindowCreateFlags rhs) { return lhs | static_cast<std::underlying_type_t<enum_class>>(rhs); }			\
-	constexpr std::underlying_type_t<enum_class> operator|(WindowCreateFlags lhs, uint32_t rhs) { return rhs | lhs; }															\
-	constexpr std::underlying_type_t<enum_class> operator|(WindowCreateFlags lhs, WindowCreateFlags rhs) { return static_cast<std::underlying_type_t<enum_class>>(lhs) | rhs; }	\
-	constexpr std::underlying_type_t<enum_class> operator|=(uint32_t& lhs, WindowCreateFlags rhs) { return lhs |= static_cast<std::underlying_type_t<enum_class>>(rhs); }
+	constexpr std::underlying_type_t<enum_class> operator|(std::underlying_type_t<enum_class> lhs, enum_class rhs) { return lhs | static_cast<std::underlying_type_t<enum_class>>(rhs); }			\
+	constexpr std::underlying_type_t<enum_class> operator|(enum_class lhs, std::underlying_type_t<enum_class> rhs) { return rhs | lhs; }															\
+	constexpr std::underlying_type_t<enum_class> operator|(enum_class lhs, enum_class rhs) { return static_cast<std::underlying_type_t<enum_class>>(lhs) | rhs; }	\
+	constexpr std::underlying_type_t<enum_class> operator|=(std::underlying_type_t<enum_class>& lhs, enum_class rhs) { return lhs |= static_cast<std::underlying_type_t<enum_class>>(rhs); }
 
 #define PW_DEFINE_ENUMCLASS_OPERATOR_AND(enum_class)	\
-	constexpr std::underlying_type_t<enum_class> operator&(uint32_t lhs, WindowCreateFlags rhs) { return lhs & static_cast<std::underlying_type_t<enum_class>>(rhs); }			\
-	constexpr std::underlying_type_t<enum_class> operator&(WindowCreateFlags lhs, uint32_t rhs) { return rhs & lhs; }															\
-	constexpr std::underlying_type_t<enum_class> operator&=(uint32_t& lhs, WindowCreateFlags rhs) { return lhs &= static_cast<std::underlying_type_t<enum_class>>(rhs); }
+	constexpr std::underlying_type_t<enum_class> operator&(std::underlying_type_t<enum_class> lhs, enum_class rhs) { return lhs & static_cast<std::underlying_type_t<enum_class>>(rhs); }			\
+	constexpr std::underlying_type_t<enum_class> operator&(enum_class lhs, std::underlying_type_t<enum_class> rhs) { return rhs & lhs; }															\
+	constexpr std::underlying_type_t<enum_class> operator&=(std::underlying_type_t<enum_class>& lhs, enum_class rhs) { return lhs &= static_cast<std::underlying_type_t<enum_class>>(rhs); }
+
+#define PW_DEFINE_ENUMCLASS_OPERATOR_EQUALS(enum_class)	\
+	constexpr std::underlying_type_t<enum_class> operator==(std::underlying_type_t<enum_class> lhs, enum_class rhs) { return lhs == static_cast<std::underlying_type_t<enum_class>>(rhs); }	\
+	constexpr std::underlying_type_t<enum_class> operator==(enum_class lhs, std::underlying_type_t<enum_class> rhs) { return rhs == lhs; }
+
+#define PW_DEFINE_ENUMCLASS_OPERATOR_NOT(enum_class)	\
+	constexpr std::underlying_type_t<enum_class> operator~(enum_class value) { return static_cast<std::underlying_type_t<enum_class>>(value); }
